5ada226658e3f9ecbea92bcbd991ad096bf0e36e
Chrisnoome <Chris.noome@gmail.com>
2025-10-23 14:01:32 +0200

    Added Delphi Project

---
+++ Project/CNPat.dpr
@@ -0,0 +1,14 @@
+program CNPat;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  ufrmMain in 'ufrmMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
